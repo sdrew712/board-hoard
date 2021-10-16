c71d68b0dbac72537f77f847f00f22855629471b
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-15 21:44:53 -0700

    add app component with react router

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+import TopBar from "./TopBar/TopBar";
+import HomePage from "./HomePage/HomePage";
+import ProductsPage from "./Products/ProductsPage";
+import Cart from "./Cart/Cart";
 
 const App = () => {
-  return <div>App :)</div>;
+  return (
+    <Router>
+      <TopBar />
+
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ProductsPage} />
+        <Route exact path="/cart" component={Cart} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;

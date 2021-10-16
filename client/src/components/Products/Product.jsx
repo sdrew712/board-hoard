757f60b334e38ae6aa64f654dd96d62f229834e0
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-16 00:46:18 -0700

    add Link to ProductPage on each board

--- client/src/components/Products/Product.jsx
+++ client/src/components/Products/Product.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ProductPage from "./ProductPage";
 
 const Product = ({ imageURL, name, price, brand, category }) => {
   return (
-    <div>
-      <img src={imageURL} alt={`${name} deck`} style={{ width: "250px" }} />
-      <p>{brand}</p>
-      <p>{category}</p>
-      <h3>{name}</h3>
-      <p>{price}</p>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path={`/shop/productpage`} component={ProductPage} />
+        </Switch>
+        <Link to={`/shop/productpage`}>
+          <img src={imageURL} alt={`${name} deck`} style={{ width: "250px" }} />
+          <p>{brand}</p>
+          <p>{category}</p>
+          <h3>{name}</h3>
+          <p>{price}</p>
+        </Link>
+      </Router>
+    </>
   );
 };
 
---
+++ client/src/components/Products/ProductPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductPage = () => {
+  return <div>prdouce page</div>;
+};
+
+export default ProductPage;

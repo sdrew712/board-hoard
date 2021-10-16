f6ddcf92bab17976b805ffe90e36bc791c49ac28
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-15 22:31:28 -0700

    add getBoardsData to Product component

--- client/src/components/Products/Product.jsx
+++ client/src/components/Products/Product.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { getBoardsData } from "../../boardsData";
+
+getBoardsData().then((res) => {
+  const fuckYou = res.data;
+  console.log(fuckYou);
+});
 
 const Product = () => {
+  const [boardsData, setBoardsData] = useState({});
+
   return <div>this is a product!</div>;
 };
 

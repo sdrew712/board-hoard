a5262d1ef3b02bf4d437b22633c096a41de64caf
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-16 00:32:20 -0700

    add Product component for each board

--- client/src/components/Products/Product.jsx
+++ client/src/components/Products/Product.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Product = () => {
-  return <div>this is a product!</div>;
+const Product = ({ imageURL, name, price, brand, category }) => {
+  return (
+    <div>
+      <img src={imageURL} alt={`${name} deck`} style={{ width: "250px" }} />
+      <p>{brand}</p>
+      <p>{category}</p>
+      <h3>{name}</h3>
+      <p>{price}</p>
+    </div>
+  );
 };
 
 export default Product;
--- client/src/components/Products/index.jsx
+++ client/src/components/Products/index.jsx
@@ -11,10 +11,31 @@ const ProductsPage = () => {
     });
   }, []);
 
-  boardsData.map(() => {});
-  console.log(boardsData);
+  const boardRenderer = boardsData.map((data) => {
+    console.log(data[1]);
+    return (
+      <Product
+        key={data[1].id}
+        name={data[1].name}
+        price={data[1].price}
+        brand={data[1].brand}
+        category={data[1].category}
+        imageURL={data[1].image_url}
+        description={data[1].description}
+        length={data[1].length}
+        width={data[1].width}
+        wheelbase={data[1].wheelbase}
+        flex={data[1].flex}
+      />
+    );
+  });
 
-  return <div>Products Page bro!</div>;
+  return (
+    <div>
+      Products Page bro!
+      <div>{boardRenderer}</div>
+    </div>
+  );
 };
 
 export default ProductsPage;

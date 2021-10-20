80b155e7f794eefcc5d537fad80950fd2eb31e39
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-20 15:30:23 -0700

    move endpoint functions into separate file

--- server/index.ts
+++ server/index.ts
@@ -1,89 +1,22 @@
-import { Request, Response } from "express";
-const express = require("express");
-const app = express();
 const cors = require("cors");
-import { PrismaClient } from "@prisma/client";
+import express from "express";
+import {
+  getAllBoards,
+  getFilteredBoards,
+  getSearchedBoards,
+} from "./scripts/retrieveData";
 
+const app = express();
 app.use(cors());
 app.use(express.json());
-const prisma = new PrismaClient();
+
+app.get("/api/boards", getAllBoards);
+
+app.get("/api/filter", getFilteredBoards);
+
+app.get("/api/search", getSearchedBoards);
 
 const port = 3001;
-
-app.get("/api/boards", async (req: Request, res: Response) => {
-  const allBoards = await prisma.boards.findMany();
-  res.status(200).send(allBoards);
-});
-
-app.get("/api/filter", async (req: Request, res: Response) => {
-  const userSelectedBrands: any = req.query.brandFilterTerms;
-  const userSelectedCategories: any = req.query.categoryFilterTerms;
-
-  const brandFilterArray: Array<any> = [];
-  const categoryFilterArray: Array<any> = [];
-
-  userSelectedBrands?.forEach((brand: string) => {
-    brandFilterArray.push({ brand: brand });
-  });
-
-  userSelectedCategories?.forEach((category: string) => {
-    categoryFilterArray.push({
-      category: {
-        contains: category,
-      },
-    });
-  });
-
-  let filteredBoards: Array<object> = [];
-
-  if (brandFilterArray.length > 0) {
-    filteredBoards = await prisma.boards.findMany({
-      where: {
-        OR: brandFilterArray,
-        AND: categoryFilterArray,
-      },
-    });
-  } else if (categoryFilterArray.length > 0) {
-    filteredBoards = await prisma.boards.findMany({
-      where: {
-        OR: categoryFilterArray,
-        AND: brandFilterArray,
-      },
-    });
-  }
-
-  res.status(200).send(filteredBoards);
-});
-
-app.get("/api/search", async (req: Request, res: Response) => {
-  const searchTerm = req.query.term;
-
-  const searchResults = await prisma.boards.findMany({
-    where: {
-      OR: [
-        {
-          name: {
-            contains: searchTerm,
-            mode: "insensitive",
-          },
-        },
-        {
-          brand: {
-            contains: searchTerm,
-            mode: "insensitive",
-          },
-        },
-        {
-          category: {
-            contains: searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
-    },
-  });
-  res.status(200).send(searchResults);
-});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
---
+++ server/scripts/retrieveData.ts
@@ -0,0 +1,78 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+const prisma = new PrismaClient();
+
+export const getAllBoards = async (req: Request, res: Response) => {
+  const allBoards = await prisma.boards.findMany();
+  res.status(200).send(allBoards);
+};
+
+export const getFilteredBoards = async (req: Request, res: Response) => {
+  const userSelectedBrands: any = req.query.brandFilterTerms;
+  const userSelectedCategories: any = req.query.categoryFilterTerms;
+
+  const brandFilterArray: Array<any> = [];
+  const categoryFilterArray: Array<any> = [];
+
+  userSelectedBrands?.forEach((brand: string) => {
+    brandFilterArray.push({ brand: brand });
+  });
+
+  userSelectedCategories?.forEach((category: string) => {
+    categoryFilterArray.push({
+      category: {
+        contains: category,
+      },
+    });
+  });
+
+  let filteredBoards: Array<object> = [];
+
+  if (brandFilterArray.length > 0) {
+    filteredBoards = await prisma.boards.findMany({
+      where: {
+        OR: brandFilterArray,
+        AND: categoryFilterArray,
+      },
+    });
+  } else if (categoryFilterArray.length > 0) {
+    filteredBoards = await prisma.boards.findMany({
+      where: {
+        OR: categoryFilterArray,
+        AND: brandFilterArray,
+      },
+    });
+  }
+
+  res.status(200).send(filteredBoards);
+};
+
+export const getSearchedBoards = async (req: Request, res: Response) => {
+  const searchTerm = req.query.term;
+
+  const searchResults = await prisma.boards.findMany({
+    where: {
+      OR: [
+        {
+          name: {
+            contains: searchTerm,
+            mode: "insensitive",
+          },
+        },
+        {
+          brand: {
+            contains: searchTerm,
+            mode: "insensitive",
+          },
+        },
+        {
+          category: {
+            contains: searchTerm,
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
+  res.status(200).send(searchResults);
+};

6f1c7f97a04d69e61c3c8f3daa308baf352f49f5
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-22 10:35:58 -0700

    fix bug where deselecting one filter would clear all

--- client/src/components/Shop/FilterButton.jsx
+++ client/src/components/Shop/FilterButton.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const FilterButton = ({ setIsFiltered, filterTerms, setFilterTerms, filterButtonText }) => {
   const handleFilterTerms = (buttonClicked) => {
+    //if button clicked is in filter terms and if there are no more buttons left, set filtered to false
+    if (filterTerms.includes(buttonClicked) && filterTerms.length <= 1) {
+      setIsFiltered(false);
+    }
     //if button clicked is the same as one in the state array, change the array to remove it from the filter state
-    setIsFiltered(false);
     if (filterTerms.includes(buttonClicked)) {
       setFilterTerms(
         filterTerms.filter((term) => {
@@ -11,7 +14,7 @@ const FilterButton = ({ setIsFiltered, filterTerms, setFilterTerms, filterButton
         })
       );
     }
-    //else if it is not a filter term yet, add it to the filter term state
+    //else if it is not a filter term yet, add it to the filter term state and set filtered to true
     else {
       setFilterTerms([...filterTerms, buttonClicked]);
       setIsFiltered(true);

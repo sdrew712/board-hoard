3546cbeb0a6bd6743b9e36434fb25352304382ac
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-22 01:32:50 -0700

    fix product page attaching to shp page

--- client/src/components/Shop/Product.jsx
+++ client/src/components/Shop/Product.jsx
@@ -1,23 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, useHistory } from "react-router-dom";
 import ProductPage from "./ProductPage";
 
 const Product = ({ imageURL, name, price, brand, category, flex }) => {
+  let history = useHistory();
+  function handleClick() {
+    history.push(`/productpage`);
+  }
   return (
     <>
-      <Router>
-        <Switch>
-          <Route exact path={`/shop/productpage`} component={ProductPage} />
-        </Switch>
-        <Link to={`/shop/productpage`}>
-          <img src={imageURL} alt={`${name} deck`} style={{ width: "250px" }} />
-          <p>{brand}</p>
-          <p>{category}</p>
-          <h3>{name}</h3>
-          <p>{price}</p>
-          <p>{flex}</p>
-        </Link>
-      </Router>
+      <div onClick={handleClick}>
+        <img src={imageURL} alt={`${name} deck`} style={{ width: "250px" }} />
+        <p>{brand}</p>
+        <p>{category}</p>
+        <h3>{name}</h3>
+        <p>{price}</p>
+        <p>{flex}</p>
+      </div>
     </>
   );
 };

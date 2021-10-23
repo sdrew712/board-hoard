0ae3cb89be9fdab3cec90da40879cd4be8f08a0b
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-23 12:37:36 -0700

    remove state passed through link to product page

--- client/src/components/Shop/Product.jsx
+++ client/src/components/Shop/Product.jsx
@@ -4,23 +4,7 @@ import { Link } from "react-router-dom";
 const Product = ({ id, imageURL, name, price, brand, category, flex, description, length, width, wheelbase }) => {
   return (
     <>
-      <Link
-        to={{
-          pathname: `/shop/${id}`,
-          state: {
-            imageURL,
-            name,
-            price,
-            brand,
-            category,
-            flex,
-            description,
-            length,
-            width,
-            wheelbase,
-          },
-        }}
-      >
+      <Link to={`/shop/${id}`}>
         <div>
           <img src={imageURL} alt={`${name} deck`} style={{ width: "250px" }} />
           <p>{brand}</p>

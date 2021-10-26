14bd6dafca5e41eaf7d94144b2947d40d882a580
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-26 12:47:08 -0700

    add submitted cart data to checkout page

--- server/scripts/checkout.ts
+++ server/scripts/checkout.ts
@@ -3,22 +3,28 @@ import stripe from "./stripe";
 
 export const checkout = async (req: Request, res: Response) => {
   const cart = JSON.parse(req.body.cart);
-  console.log(cart);
 
+  const lineItems: Array<object> = [];
+
+  //iterate over data to insert mutliple values to cart
+  cart.forEach((cartItem: any) => {
+    lineItems.push({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: cartItem.productName,
+        },
+        //stripe requires cents, so change to cent amount
+        unit_amount: cartItem.productPrice * 100,
+      },
+      quantity: cartItem.quantity,
+    });
+  });
+
+  //create stripe session and redirect user to it
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "T-shirt",
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
+    line_items: lineItems,
     mode: "payment",
     success_url: "http://localhost:3000/success",
     cancel_url: "http://localhost:3000/cart",

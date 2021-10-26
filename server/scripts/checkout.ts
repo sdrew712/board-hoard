1da85e1f4b50627e3a491557b4b7b63610f31fd7
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-26 11:47:09 -0700

    fix checkout in backend

--- server/scripts/checkout.ts
+++ server/scripts/checkout.ts
@@ -1,32 +1,9 @@
 import { Request, Response } from "express";
-import { getAllBoards } from "./retrieveData";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-
-import { env } from "process";
-const { SK_TEST } = env;
-
-// @ts-ignore
-const stripe = require("stripe")(SK_TEST);
+import stripe from "./stripe";
 
 export const checkout = async (req: Request, res: Response) => {
-  const boards = await prisma.boards.findMany();
-
-  // console.log(boards);
-
-  boards.forEach(async (board) => {
-    // const product = await stripe.products.create({
-    //   id: board.id,
-    //   name: board.name,
-    // });
-    // console.log(product);
-    // const price = await stripe.prices.create({
-    //   product: board.id,
-    //   unit_amount: board.price,
-    //   currency: "usd",
-    // });
-    // console.log(price);
-  });
+  const cart = JSON.parse(req.body.cart);
+  console.log(cart);
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],

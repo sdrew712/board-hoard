1cd69850604b9ec13d3ef39ceedb6172d1377383
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-26 11:48:18 -0700

    add checkout to frontend

--- client/src/components/Cart/Checkout.jsx
+++ client/src/components/Cart/Checkout.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 
 const Checkout = () => {
+  const stringCart = localStorage.getItem("cart");
+  const cart = JSON.parse(stringCart);
+
+  const cartItems = cart.map((item) => {
+    return (
+      <div key={item.productId}>
+        <h2>{item.productName}</h2>
+        <p>${item.productPrice}</p>
+        <p>Quantity: {item.quantity}</p>
+      </div>
+    );
+  });
+
   return (
-    <form action="http://localhost:3001/create-checkout-session" method="POST">
-      <button type="submit">Checkout</button>
-    </form>
+    <>
+      <form action="http://localhost:3001/create-checkout-session" method="POST">
+        {cartItems}
+        <input type="hidden" name="cart" value={stringCart} />
+        <button type="submit">Checkout</button>
+      </form>
+    </>
   );
 };
 

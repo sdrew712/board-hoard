4a0323d8eea7a6c1a20484f8718bda39323a7ae1
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-26 14:35:31 -0700

    remove Checkout component, place into Cart index

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -1,8 +1,28 @@
 import React from "react";
-import Checkout from "./Checkout";
 
 const Cart = () => {
-  return <Checkout />;
+  const stringCart = localStorage.getItem("cart");
+  const cart = JSON.parse(stringCart);
+
+  const cartItems = cart.map((item) => {
+    return (
+      <div key={item.productId}>
+        <h2>{item.productName}</h2>
+        <p>${item.productPrice}</p>
+        <p>Quantity: {item.quantity}</p>
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <form action="http://localhost:3001/create-checkout-session" method="POST">
+        {cartItems}
+        <input type="hidden" name="cart" value={stringCart} />
+        <button type="submit">Checkout</button>
+      </form>
+    </>
+  );
 };
 
 export default Cart;

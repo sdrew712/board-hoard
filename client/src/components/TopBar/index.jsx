a7660cf715a3201831db6a41bfc1250b13ea6d16
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-27 17:21:43 -0700

    update topbar to mui

--- client/src/components/TopBar/index.jsx
+++ client/src/components/TopBar/index.jsx
@@ -1,22 +1,44 @@
 import React from "react";
-import Headroom from "react-headroom";
 import { NavLink } from "react-router-dom";
 
-const TopBar = () => {
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Slide from "@mui/material/Slide";
+
+function HideOnScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger();
+
   return (
-    <Headroom>
-      <nav>
-        <NavLink to="/" exact activeClassName="active">
-          Board Hoard
-        </NavLink>
-        <NavLink to="/shop" exact activeClassName="active">
-          Shop
-        </NavLink>
-        <NavLink to="/cart" exact activeClassName="active">
-          Cart
-        </NavLink>
-      </nav>
-    </Headroom>
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+const TopBar = (props) => {
+  return (
+    <HideOnScroll {...props}>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            <nav>
+              <NavLink to="/" exact activeClassName="active">
+                Board Hoard
+              </NavLink>
+              <NavLink to="/shop" exact activeClassName="active">
+                Shop
+              </NavLink>
+              <NavLink to="/cart" exact activeClassName="active">
+                Cart
+              </NavLink>
+            </nav>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </HideOnScroll>
   );
 };
 

bc79ab63f554e78aa5b6e559f5396232b51b7bbe
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-27 17:06:11 -0700

    update mui theme

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -15,6 +15,9 @@ import NotFoundPage from "./NotFoundPage/NotFoundPage";
 const theme = createTheme({
   palette: {
     primary: {
+      main: "#FFFFFF",
+    },
+    secondary: {
       main: "#f67671",
     },
   },

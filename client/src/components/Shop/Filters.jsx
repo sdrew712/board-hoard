022835c1463f472544272083bb2d7d03e4c0629d
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-27 22:19:56 -0700

    fix dom nesting error

--- client/src/components/Shop/Filters.jsx
+++ client/src/components/Shop/Filters.jsx
@@ -165,99 +165,97 @@ const Filters = ({ getBoardsData, setBoardsData, isFiltered, setIsFiltered }) =>
               <Typography>Filters</Typography>
             </AccordionSummary>
             <AccordionDetails>
-              <Typography>
+              <Typography component={"span"}>
                 <button onClick={handleClearFilters}>Clear filters</button>
-                <div>
-                  <Search
-                    searchTerm={searchTerm}
-                    setSearchTerm={setSearchTerm}
-                    setIsFiltered={setIsFiltered}
-                    setDebouncedSearchTerm={setDebouncedSearchTerm}
-                  />
+                <Search
+                  searchTerm={searchTerm}
+                  setSearchTerm={setSearchTerm}
+                  setIsFiltered={setIsFiltered}
+                  setDebouncedSearchTerm={setDebouncedSearchTerm}
+                />
 
-                  <h2>brand</h2>
-                  <FilterButton
-                    setIsFiltered={setIsFiltered}
-                    filterTerms={brandFilterTerms}
-                    setFilterTerms={setBrandFilterTerms}
-                    filterButtonText="Landyachtz"
-                  />
-                  <FilterButton
-                    setIsFiltered={setIsFiltered}
-                    filterTerms={brandFilterTerms}
-                    setFilterTerms={setBrandFilterTerms}
-                    filterButtonText="Zenit"
-                  />
-                  <FilterButton
-                    setIsFiltered={setIsFiltered}
-                    filterTerms={brandFilterTerms}
-                    setFilterTerms={setBrandFilterTerms}
-                    filterButtonText="Moonshine MFG"
-                  />
-                  <FilterButton
-                    setIsFiltered={setIsFiltered}
-                    filterTerms={brandFilterTerms}
-                    setFilterTerms={setBrandFilterTerms}
-                    filterButtonText="Loaded"
-                  />
-                  <FilterButton
-                    setIsFiltered={setIsFiltered}
-                    filterTerms={brandFilterTerms}
-                    setFilterTerms={setBrandFilterTerms}
-                    filterButtonText="Rayne"
-                  />
+                <h2>brand</h2>
+                <FilterButton
+                  setIsFiltered={setIsFiltered}
+                  filterTerms={brandFilterTerms}
+                  setFilterTerms={setBrandFilterTerms}
+                  filterButtonText="Landyachtz"
+                />
+                <FilterButton
+                  setIsFiltered={setIsFiltered}
+                  filterTerms={brandFilterTerms}
+                  setFilterTerms={setBrandFilterTerms}
+                  filterButtonText="Zenit"
+                />
+                <FilterButton
+                  setIsFiltered={setIsFiltered}
+                  filterTerms={brandFilterTerms}
+                  setFilterTerms={setBrandFilterTerms}
+                  filterButtonText="Moonshine MFG"
+                />
+                <FilterButton
+                  setIsFiltered={setIsFiltered}
+                  filterTerms={brandFilterTerms}
+                  setFilterTerms={setBrandFilterTerms}
+                  filterButtonText="Loaded"
+                />
+                <FilterButton
+                  setIsFiltered={setIsFiltered}
+                  filterTerms={brandFilterTerms}
+                  setFilterTerms={setBrandFilterTerms}
+                  filterButtonText="Rayne"
+                />
 
-                  <h2>category</h2>
-                  <FilterButton
-                    setIsFiltered={setIsFiltered}
-                    filterTerms={categoryFilterTerms}
-                    setFilterTerms={setCategoryFilterTerms}
-                    filterButtonText="Dance"
-                  />
-                  <FilterButton
-                    setIsFiltered={setIsFiltered}
-                    filterTerms={categoryFilterTerms}
-                    setFilterTerms={setCategoryFilterTerms}
-                    filterButtonText="Freestyle"
-                  />
-                  <FilterButton
-                    setIsFiltered={setIsFiltered}
-                    filterTerms={categoryFilterTerms}
-                    setFilterTerms={setCategoryFilterTerms}
-                    filterButtonText="Downhill"
-                  />
+                <h2>category</h2>
+                <FilterButton
+                  setIsFiltered={setIsFiltered}
+                  filterTerms={categoryFilterTerms}
+                  setFilterTerms={setCategoryFilterTerms}
+                  filterButtonText="Dance"
+                />
+                <FilterButton
+                  setIsFiltered={setIsFiltered}
+                  filterTerms={categoryFilterTerms}
+                  setFilterTerms={setCategoryFilterTerms}
+                  filterButtonText="Freestyle"
+                />
+                <FilterButton
+                  setIsFiltered={setIsFiltered}
+                  filterTerms={categoryFilterTerms}
+                  setFilterTerms={setCategoryFilterTerms}
+                  filterButtonText="Downhill"
+                />
 
-                  <h2>flex</h2>
-                  <FilterButton
-                    setIsFiltered={setIsFiltered}
-                    filterTerms={flexFilterTerms}
-                    setFilterTerms={setFlexFilterTerms}
-                    filterButtonText="Soft"
-                  />
-                  <FilterButton
-                    setIsFiltered={setIsFiltered}
-                    filterTerms={flexFilterTerms}
-                    setFilterTerms={setFlexFilterTerms}
-                    filterButtonText="Medium"
-                  />
-                  <FilterButton
-                    setIsFiltered={setIsFiltered}
-                    filterTerms={flexFilterTerms}
-                    setFilterTerms={setFlexFilterTerms}
-                    filterButtonText="Stiff"
-                  />
+                <h2>flex</h2>
+                <FilterButton
+                  setIsFiltered={setIsFiltered}
+                  filterTerms={flexFilterTerms}
+                  setFilterTerms={setFlexFilterTerms}
+                  filterButtonText="Soft"
+                />
+                <FilterButton
+                  setIsFiltered={setIsFiltered}
+                  filterTerms={flexFilterTerms}
+                  setFilterTerms={setFlexFilterTerms}
+                  filterButtonText="Medium"
+                />
+                <FilterButton
+                  setIsFiltered={setIsFiltered}
+                  filterTerms={flexFilterTerms}
+                  setFilterTerms={setFlexFilterTerms}
+                  filterButtonText="Stiff"
+                />
 
-                  <h2>Sort by</h2>
-                  <DropDown
-                    labelOptions={[
-                      { text: "Lowest Price", value: "asc" },
-                      { text: "Highest Price", value: "desc" },
-                    ]}
-                    filterTerm={sortByTerm}
-                    setFilterTerm={setSortByTerm}
-                    setIsFiltered={setIsFiltered}
-                  />
-                </div>
+                <h2>Sort by</h2>
+                <DropDown
+                  labelOptions={[
+                    { text: "Lowest Price", value: "asc" },
+                    { text: "Highest Price", value: "desc" },
+                  ]}
+                  filterTerm={sortByTerm}
+                  setFilterTerm={setSortByTerm}
+                  setIsFiltered={setIsFiltered}
+                />
               </Typography>
             </AccordionDetails>
           </Accordion>

3e2b8411336c62d0b1a6e4468f7a9e80098f87ee
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-28 08:20:30 -0700

    update dropdown to inputLabel

--- client/src/components/Shop/DropDown.jsx
+++ client/src/components/Shop/DropDown.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
 const DropDown = ({ labelText, labelOptions, filterTerm, setFilterTerm, setIsFiltered }) => {
   //map over labelOptions and return a list of options
-  const options = labelOptions.map((option) => {
-    return (
-      <option key={option.value} value={option.value}>
-        {option.text}
-      </option>
-    );
+  const menuItems = labelOptions.map((option) => {
+    return <MenuItem value={option.value}>{option.text}</MenuItem>;
   });
 
   //when the user selects an option, set the filterTerm to the selected option's value
-  const handleDropdownChange = (value) => {
-    if (value) {
-      setFilterTerm(value);
+  const handleDropdownChange = ({ target }) => {
+    if (target.value) {
+      setFilterTerm(target.value);
       setIsFiltered(true);
     }
     //if the user selects the empty option, set the filterTerm to an empty string
@@ -25,17 +26,21 @@ const DropDown = ({ labelText, labelOptions, filterTerm, setFilterTerm, setIsFil
 
   return (
     <>
-      <select
-        name={labelText}
-        id={labelText}
-        value={filterTerm}
-        onChange={({ target }) => {
-          handleDropdownChange(target.value);
-        }}
-      >
-        <option />
-        {options}
-      </select>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-standard-label">Sort by</InputLabel>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={labelText}
+          onChange={handleDropdownChange}
+          label={labelText}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {menuItems}
+        </Select>
+      </FormControl>
     </>
   );
 };

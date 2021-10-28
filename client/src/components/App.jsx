89ce4b4b1894626c88733e3f9f13f7fec2121a75
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-27 22:09:54 -0700

    fix page text going under navbar bug

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -29,19 +29,21 @@ const App = () => {
       <Router>
         <TopBar />
 
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/shop/:id" component={ProductPage} />
-          <Route exact path="/cart" component={Cart} />
-          <Route exact path="/cart" component={Cart} />
-          <Route exact path="/success" component={Success} />
-          <Route path="/*">
-            <Redirect to="/" />
-            <Route path="/404" component={NotFoundPage} />
-            <Redirect to="/404" />
-          </Route>
-        </Switch>
+        <div style={{ paddingTop: "56px" }}>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/shop" component={ShopPage} />
+            <Route exact path="/shop/:id" component={ProductPage} />
+            <Route exact path="/cart" component={Cart} />
+            <Route exact path="/cart" component={Cart} />
+            <Route exact path="/success" component={Success} />
+            <Route path="/*">
+              <Redirect to="/" />
+              <Route path="/404" component={NotFoundPage} />
+              <Redirect to="/404" />
+            </Route>
+          </Switch>
+        </div>
       </Router>
     </ThemeProvider>
   );

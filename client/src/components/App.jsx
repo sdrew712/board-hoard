fe331225a3fb5c48c6dd7be69ca3bfac76f60ccf
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-27 17:04:57 -0700

    add mui ThemeProvider

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import "../index.scss";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import TopBar from "./TopBar";
 import HomePage from "./HomePage";
@@ -10,25 +11,36 @@ import ProductPage from "./Shop/ProductPage";
 import Success from "./Cart/Success";
 import NotFoundPage from "./NotFoundPage/NotFoundPage";
 
+//create mui theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f67671",
+    },
+  },
+});
+
 const App = () => {
   return (
-    <Router>
-      <TopBar />
+    <ThemeProvider theme={theme}>
+      <Router>
+        <TopBar />
 
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/shop/:id" component={ProductPage} />
-        <Route exact path="/cart" component={Cart} />
-        <Route exact path="/cart" component={Cart} />
-        <Route exact path="/success" component={Success} />
-        <Route path="/*">
-          <Redirect to="/" />
-          <Route path="/404" component={NotFoundPage} />
-          <Redirect to="/404" />
-        </Route>
-      </Switch>
-    </Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/shop/:id" component={ProductPage} />
+          <Route exact path="/cart" component={Cart} />
+          <Route exact path="/cart" component={Cart} />
+          <Route exact path="/success" component={Success} />
+          <Route path="/*">
+            <Redirect to="/" />
+            <Route path="/404" component={NotFoundPage} />
+            <Redirect to="/404" />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 };
 

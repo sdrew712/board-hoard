5a4549e45a60dcab69983e4609e3df038f371206
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-29 12:03:53 -0700

    add video to homepage

--- client/src/components/HomePage/NavBox.jsx
+++ client/src/components/HomePage/NavBox.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import video from "../videos/longebaord.mp4";
 
-const NavBox = ({ text, nav }) => {
-  if (nav) {
-    return (
-      <Link to={`/shop?category=${text}`}>
-        <div style={{ backgroundColor: "aqua" }}>
-          <h2>{text}</h2>
-        </div>
-      </Link>
-    );
-  } else {
-    return (
-      <div style={{ backgroundColor: "aqua" }}>
-        <h2>{text}</h2>
-      </div>
-    );
-  }
+const NavBox = () => {
+  return (
+    <Link to={`/shop`}>
+      <video className="videoTag" autoPlay loop muted>
+        <source src={video} type="video/mp4" />
+      </video>
+      <h1>Up your longboarding game</h1>
+    </Link>
+  );
 };
 
 export default NavBox;
--- client/src/components/HomePage/index.jsx
+++ client/src/components/HomePage/index.jsx
@@ -4,11 +4,7 @@ import NavBox from "./NavBox";
 const HomePage = () => {
   return (
     <div>
-      homepage
-      <NavBox text="Up your longboarding game" />
-      <NavBox text="Freestyle" nav={true} />
-      <NavBox text="Dance" nav={true} />
-      <NavBox text="Downhill" nav={true} />
+      <NavBox />
     </div>
   );
 };

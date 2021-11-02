75dd34561c359c40253114b1a0e53e40cba80789
Sarah Drew <drew.sarah.e@gmail.com>
2021-11-02 11:19:12 -0700

    add success page css

--- client/src/components/Cart/Success.jsx
+++ client/src/components/Cart/Success.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Success = () => {
   return (
-    <div>
+    <div id="success-page">
       <h1>Thanks for your order!</h1>
       <p>
         We appreciate your business! If you have any questions, please email
-        <a href="mailto:orders@example.com">orders@example.com</a>.
+        <a href="mailto:orders@example.com"> orders@example.com</a>.
       </p>
     </div>
   );

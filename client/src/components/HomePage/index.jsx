9289df012cc7be95f8412ba41570cce877191e2f
Sarah Drew <drew.sarah.e@gmail.com>
2021-11-02 16:02:48 -0700

    update home background to image for mobile

--- client/src/components/HomePage/index.jsx
+++ client/src/components/HomePage/index.jsx
@@ -13,7 +13,7 @@ const HomePage = () => {
     <>
       {width > 768 ? (
         <div className="home-container">
-          <video className="video" autoPlay loop muted>
+          <video className="home-media" autoPlay loop muted>
             <source src={video} type="video/mp4" />
           </video>
 
@@ -27,6 +27,11 @@ const HomePage = () => {
         </div>
       ) : (
         <div className="home-container">
+          <img
+            className="home-media"
+            src="https://images.unsplash.com/photo-1569082573633-6c411d829597?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+            alt="girl riding longboard"
+          />
           <div className="slogan-container">
             <h1 id="slogan">Up your longboarding game</h1>
 

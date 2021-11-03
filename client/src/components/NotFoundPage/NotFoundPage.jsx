37bd77b43416e8cd6b5412823d0a30f7e9b9a000
Sarah Drew <drew.sarah.e@gmail.com>
2021-11-03 14:51:10 -0700

    update 404 page

--- client/src/components/NotFoundPage/NotFoundPage.jsx
+++ client/src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import "./index.scss";
+
 const NotFoundPage = () => {
-  return <div>404 bro, can't find the page you want</div>;
+  return (
+    <div id="not-found-page">
+      <h1 className="header">404 Not Found</h1>
+      Can't find the page you want bro.
+    </div>
+  );
 };
 
 export default NotFoundPage;
